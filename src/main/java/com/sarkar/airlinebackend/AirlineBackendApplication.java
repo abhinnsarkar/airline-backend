25fca3c47c0564071f37d8ab904dde0b49c3f93c
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-03-24 21:36:31 -0700

    converted to gradle with swagger support

--- src/main/java/com/sarkar/airlinebackend/AirlineBackendApplication.java
+++ src/main/java/com/sarkar/airlinebackend/AirlineBackendApplication.java
@@ -2,14 +2,8 @@ package com.sarkar.airlinebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-//@EnableSwagger2WebMvc
-//@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
-@EnableSwagger2
 public class AirlineBackendApplication {
 
 	public static void main(String[] args) {
