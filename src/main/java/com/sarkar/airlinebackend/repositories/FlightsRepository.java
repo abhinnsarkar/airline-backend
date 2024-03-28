44131a16b8dc1855427ee23abaedabeee2060fff
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-03-28 16:28:15 -0700

    fixed improper arguement type

--- src/main/java/com/sarkar/airlinebackend/repositories/FlightsRepository.java
+++ src/main/java/com/sarkar/airlinebackend/repositories/FlightsRepository.java
@@ -3,17 +3,19 @@ package com.sarkar.airlinebackend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sarkar.airlinebackend.models.FlightModel;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.UUID;
 
 public interface FlightsRepository extends JpaRepository<FlightModel, String> {
 
     List<FlightModel> findByFlightNumber(String flightNumber);
 
-    List<FlightModel> findByFlightId(String flightId);
+    List<FlightModel> findByFlightId(UUID flightId);
 
-    List<FlightModel> findByRouteId(String routeId);
+    List<FlightModel> findByRouteId(UUID routeId);
 
-    List<FlightModel> findByDepartureDate(String departureDate);
+    List<FlightModel> findByDepartureDate(Date departureDate);
 
     // You can add more query methods based on your requirements
 }
