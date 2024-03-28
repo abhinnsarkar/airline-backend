085e749217596a5a02590c51bb52c04222b8e4d7
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-03-27 17:43:31 -0700

    fixed response and db save for customer post

--- src/main/java/com/sarkar/airlinebackend/CustomerEndpoint.java
+++ src/main/java/com/sarkar/airlinebackend/CustomerEndpoint.java
@@ -6,10 +6,12 @@ import com.sarkar.airlinebackend.repository.CustomerRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/customers")
@@ -27,8 +29,19 @@ public class CustomerEndpoint {
 
     @PostMapping
     @Operation(summary = "Create a new customer", description = "Create a new customer.")
-    public ResponseEntity<String> postCustomer(@RequestBody CustomerModel customer){
-        return ResponseEntity.ok("User created successfully");
+    public ResponseEntity<CustomerModel> postCustomer(@RequestBody CustomerModel customer) {
+        System.out.println(customer);
+        customer.setCustomerId(UUID.randomUUID());
+
+        System.out.println(customer);
+
+        CustomerModel savedCustomer = customerRepository.save(customer);
+
+        if (savedCustomer != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedCustomer);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
