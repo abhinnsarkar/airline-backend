505bcd35676b602a96c4b288a95f4500cff8ef07
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-04-13 15:33:13 -0700

    adjusted repo for flight interaction

--- src/main/java/com/sarkar/airlinebackend/repositories/FlightsRepository.java
+++ src/main/java/com/sarkar/airlinebackend/repositories/FlightsRepository.java
@@ -2,6 +2,8 @@ package com.sarkar.airlinebackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sarkar.airlinebackend.models.FlightModel;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jdbc.repository.query.Query;
 
 import java.sql.Date;
 import java.util.List;
@@ -17,5 +19,6 @@ public interface FlightsRepository extends JpaRepository<FlightModel, String> {
 
     List<FlightModel> findByDepartureDate(Date departureDate);
 
-    // You can add more query methods based on your requirements
+    @Query("SELECT f FROM Flight f JOIN f.route r WHERE r.originAirportCode = :airportCode")
+    List<FlightModel> findFlightsByOriginAirportCode(@Param("airportCode") String airportCode);
 }
